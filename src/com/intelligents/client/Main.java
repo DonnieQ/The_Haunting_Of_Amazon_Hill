2e4a3ec9414bda14cfcd7b020dc61a8291615a90
ChrisRuaboro <52970508+ChrisRuaboro@users.noreply.github.com>
2021-01-27 13:19:49 -1000

    Randomized current ghost with each new game instance

--- src/com/intelligents/client/Main.java
+++ src/com/intelligents/client/Main.java
@@ -8,8 +8,12 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         Game g = new Game();
-        g.populateGhostList();
-        g.print();
+        g.printGhosts();
+        for (int i = 0; i < 15; i++) {
+            System.out.println(g.getRandomGhost().getName());
+        }
+        System.out.println(g.getCurrentGhost());
+
 ////        Map g = new Map();
 ////        String banner = Files.readString(Path.of("The_Haunting_Of_Amazon_Hill/resources", "introText"));
 ////        System.out.println(banner);
--- src/com/intelligents/haunting/Game.java
+++ src/com/intelligents/haunting/Game.java
@@ -1,12 +1,22 @@
 package com.intelligents.haunting;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
     World world = new World();
-    private ArrayList<Ghost> ghosts = new ArrayList<>();
+    private List<Ghost> ghosts = new ArrayList<>();
+
     private Ghost currentGhost;
+
+    private Random r = new Random();
+
+    public Game() {
+        populateGhostList();
+        setCurrentGhost(getRandomGhost());
+    }
 
     void start() {
         boolean isValidInput;
@@ -89,12 +99,34 @@ public class Game {
     }
 
     public void populateGhostList() {
-        ghosts.addAll(XMLParser.populateGhosts(XMLParser.readGhosts()));
+        this.setGhosts(XMLParser.populateGhosts(XMLParser.readGhosts()));
     }
 
-    public void print() {
+    public void printGhosts() {
         for (Ghost ghost : ghosts) {
             System.out.println(ghost);
         }
     }
+    public Ghost getRandomGhost() {
+        int index = r.nextInt(ghosts.size());
+        return ghosts.get(index);
+    }
+
+    // Getters / Setters
+
+    public List<Ghost> getGhosts() {
+        return ghosts;
+    }
+
+    public void setGhosts(List<Ghost> ghosts) {
+        this.ghosts = ghosts;
+    }
+    public Ghost getCurrentGhost() {
+        return currentGhost;
+    }
+    public void setCurrentGhost(Ghost ghost) {
+        this.currentGhost = ghost;
+    }
+
+
 }
