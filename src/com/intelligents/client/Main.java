cc8b723939988169a5996669447f7b604082326c
Collin Fairlie <collin.fairlie@gmail.com>
2021-02-09 22:07:56 -0800

    Reverted back to original until code for parser and inputs can be resolved before adding the jframe.

--- src/com/intelligents/client/Main.java
+++ src/com/intelligents/client/Main.java
@@ -1,21 +1,13 @@
 package com.intelligents.client;
 
 
-import com.intelligents.haunting.HauntingJFrame;
 import com.intelligents.haunting.SplashScreen;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.MalformedURLException;
-
 public class Main implements java.io.Serializable {
-    public static void main(String[] args) throws IOException {
-//        SplashScreen s = new SplashScreen();
-//        s.pressEnterToContinue();
-//        s.splash();
-
-        HauntingJFrame jFrame = new HauntingJFrame();
+    public static void main(String[] args) {
+        SplashScreen s = new SplashScreen();
+        s.pressEnterToContinue();
+        s.splash();
 
     }
 }
-
