1fb44ad8021bc5959c9ce953feaab80f28baf5d2
Collin Fairlie <collin.fairlie@gmail.com>
2021-02-09 20:48:47 -0800

    Got the splash screen to properly display the image and disappear after 5 seconds; to then display the main game screen.

--- src/com/intelligents/haunting/HauntingJFrame.java
+++ src/com/intelligents/haunting/HauntingJFrame.java
@@ -1,13 +1,13 @@
 package com.intelligents.haunting;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.text.DefaultCaret;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.*;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class HauntingJFrame extends JWindow implements ActionListener{
 
@@ -18,11 +18,12 @@ public class HauntingJFrame extends JWindow implements ActionListener{
     JScrollPane scrollPane;
     boolean bossDead = false;
     String result;
-    JLabel inputFromUser = new JLabel();
-    JTextField textField = new JTextField();
+    JLabel questionToUser = new JLabel();
+    JTextField userInput = new JTextField();
     JButton showJournal = new JButton("Journal");
     JButton showMap = new JButton("Map");
-    JTextArea textDisplay;
+    JTextArea textDisplayGameWindow = new JTextArea();
+    JTextArea textDisplayJournal = new JTextArea();
     JTextField desicionField = new JTextField();
     boolean decisionListener = false;
     JFrame frame;
@@ -32,76 +33,125 @@ public class HauntingJFrame extends JWindow implements ActionListener{
     JPanel panel_03;
     JPanel panel_04;
     JTextField numInput;
+    boolean calledOnce=false;
 
 
     public HauntingJFrame() throws IOException {
         splashWindow();
+        gameWindow();
 
+    }
+
+
+    private void gameWindow() {
         frame = new JFrame("The Haunting of Amazon Hill");
-        frame.setSize(500, 500);
+        frame.setSize(700, 700);
 
+        panel_00 = new JPanel();
         panel_01 = new JPanel();
         panel_02 = new JPanel();
 
 
-        panel_01.setBackground(Color.black);
+        panel_00.setBackground(Color.black);
         panel_02.setBackground(Color.DARK_GRAY);
         panel_02.add(showJournal);
+        panel_02.add(Box.createHorizontalGlue());
+        panel_02.add(showMap);
 
         showJournal.addActionListener(this);
+        showMap.addActionListener(this);
 
-        textDisplay = new JTextArea();
-        DefaultCaret caret = (DefaultCaret) textDisplay.getCaret();
+        // Unchangeable to user, a textbox to display game text
+        DefaultCaret caret = (DefaultCaret) textDisplayGameWindow.getCaret();
         caret.setUpdatePolicy(DefaultCaret.NEVER_UPDATE);
-        textDisplay.setCaretPosition(0);
-        textDisplay.setText("This is a placeholder text");
-        textDisplay.setLineWrap(true);
-        textDisplay.setWrapStyleWord(true);
-        textDisplay.setBorder(BorderFactory.createBevelBorder(1));
-        textDisplay.setForeground(new Color(0, 60, 70));
-        textDisplay.setFont(new Font("Comic Sans",Font.BOLD, 15));
-        textDisplay.setEditable(false);
-        textDisplay.setBackground(new Color(196, 223, 230));
+        textDisplayGameWindow.setCaretPosition(0);
+        textDisplayGameWindow.setText("What game would you like to play?\n " +
+                "Chapter 1. The Haunting of Amazon Hill\n " +
+                "Chapter 2. Chasing Ghosts (COMING SOON!)\n " +
+                "Chapter 3. Hangman's Gallows (COMING SOON!)\n " +
+                "Press 4. to load saved game\n" +
+                "Please enter a number for Chapter: ");
+        textDisplayGameWindow.setLineWrap(true);
+        textDisplayGameWindow.setWrapStyleWord(true);
+        textDisplayGameWindow.setBorder(BorderFactory.createBevelBorder(1));
+        textDisplayGameWindow.setForeground(Color.white);
+        textDisplayGameWindow.setFont(new Font("Comic Sans",Font.BOLD, 15));
+        textDisplayGameWindow.setEditable(false);
+        textDisplayGameWindow.setBackground(Color.DARK_GRAY);
 
+        // Allows for scrolling is text extends panel
+        JScrollPane scrollPane = new JScrollPane(textDisplayGameWindow);
+        scrollPane.setPreferredSize(new Dimension(700,500));
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+        // Text field for user to input
+//        userInput.setText("What do you want to do?");
+        userInput.setSize(new Dimension(500,100));
+        userInput.setFont(new Font("Consolas", Font.CENTER_BASELINE, 15));
+        userInput.setForeground(Color.white);
+        userInput.setBackground(Color.DARK_GRAY);
+        userInput.setCaretColor(Color.BLACK);
+
+        panel_01.setBackground(Color.BLACK);
+        panel_00.add(scrollPane);
+        panel_01.setLayout(new GridLayout(1,2));
+//        panel_01.add(questionToUser);
+//        panel_01.add(Box.createHorizontalGlue());
+        panel_01.add(userInput);
+
+        frame.add(panel_00, BorderLayout.NORTH);
         frame.add(panel_01, BorderLayout.CENTER);
-        frame.add(panel_02, BorderLayout.PAGE_END);
+        frame.add(panel_02, BorderLayout.SOUTH);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
+        userInput.addActionListener(this);
+        userInput.requestFocusInWindow();
+
         frame.setVisible(true);
     }
 
-
-
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getActionCommand() == "Journal") {
+        if (e.getActionCommand().equals("Journal")) {
             showJournal();
         }
-        if (e.getActionCommand() == "Map") {
-            showMap();
+        if (e.getActionCommand().equals("Map")) {
+            try {
+                showMap();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        }
+        if (e.getSource() == userInput) {
+            System.out.println(userInput.getText());
+            userInput.setText("");
         }
 
+    }
+
+    public void setTextbox(String text) {
+        textDisplayGameWindow.setText(text);
     }
 
     private void showJournal() {
         frame = new JFrame("Journal");
         frame.setSize(500, 500);
 
-        textDisplay = new JTextArea();
-        DefaultCaret caret = (DefaultCaret) textDisplay.getCaret();
+        textDisplayJournal = new JTextArea();
+        DefaultCaret caret = (DefaultCaret) textDisplayJournal.getCaret();
         caret.setUpdatePolicy(DefaultCaret.NEVER_UPDATE);
-        textDisplay.setCaretPosition(0);
-        textDisplay.setText("This is your journal! There is text added here... blah blah blah");
-        textDisplay.setLineWrap(true);
-        textDisplay.setWrapStyleWord(true);
-        textDisplay.setBorder(BorderFactory.createBevelBorder(1));
-        textDisplay.setForeground(new Color(0, 60, 70));
-        textDisplay.setFont(new Font("Comic Sans",Font.BOLD, 15));
-        textDisplay.setEditable(false);
-        textDisplay.setBackground(new Color(196, 223, 230));
+        textDisplayJournal.setCaretPosition(0);
+        textDisplayJournal.setText("This is your journal! There is text added here... blah blah blah");
+        textDisplayJournal.setLineWrap(true);
+        textDisplayJournal.setWrapStyleWord(true);
+        textDisplayJournal.setBorder(BorderFactory.createBevelBorder(1));
+        textDisplayJournal.setForeground(new Color(0, 60, 70));
+        textDisplayJournal.setFont(new Font("Comic Sans",Font.BOLD, 15));
+        textDisplayJournal.setEditable(false);
+        textDisplayJournal.setBackground(new Color(196, 223, 230));
 
-        frame.add(textDisplay, BorderLayout.CENTER);
+        frame.add(textDisplayJournal, BorderLayout.CENTER);
 
 //        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 //        window.pack();
@@ -109,14 +159,15 @@ public class HauntingJFrame extends JWindow implements ActionListener{
         frame.setVisible(true);
     }
 
-    private void showMap() {
+    private void showMap() throws IOException {
         frame = new JFrame("Map");
         frame.setSize(500, 500);
 
-        textDisplay = new JTextArea();
+        BufferedImage mapImage = ImageIO.read(new File("resources/Images/Map(Lobby).png"));
+        JLabel picLabel = new JLabel(new ImageIcon(mapImage));
 
 
-        frame.add(textDisplay, BorderLayout.CENTER);
+        frame.add(picLabel, BorderLayout.CENTER);
 
 //        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 //        window.pack();
@@ -125,36 +176,45 @@ public class HauntingJFrame extends JWindow implements ActionListener{
     }
 
     private void splashWindow() throws IOException {
-        // Read the ascii file line by line and build it into a string
-        FileInputStream asciiSplashScreen = new FileInputStream("resources/splashScreen");
-        DataInputStream in = new DataInputStream(asciiSplashScreen);
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
-        StringBuilder sb = new StringBuilder();
-        String strLine;
-//        JLabel ascii = new JLabel("");
-        while ((strLine = br.readLine()) != null)   {
-            // Print the content on the console
-            System.out.println (strLine);
-            sb.append(strLine);
-//            JLabel ascii = new JLabel(String.valueOf(sb));
+//        // Read the ascii file line by line and build it into a string
+//        FileInputStream asciiSplashScreen = new FileInputStream("resources/splashScreen");
+//        DataInputStream in = new DataInputStream(asciiSplashScreen);
+//        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+//        StringBuilder sb = new StringBuilder();
+//        String strLine;
+////        JLabel ascii = new JLabel("");
+//        while ((strLine = br.readLine()) != null)   {
+//            // Print the content on the console
+//            System.out.println (strLine);
+//            sb.append(strLine);
+//            JTextField ascii = new JTextField(String.valueOf(sb));
 //            JPanel panel = new JPanel();
 //            ascii.setFont(new Font("Monospaced", Font.PLAIN, 12));
 ////        ascii.setBorder(BorderFactory.createEmptyBorder(0, 20, 20, 20));
 //            ascii.setSize(1000, 1000);
+//            panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 //            panel.add(ascii);
+//            panel.setVisible(true);
 //            window.getContentPane().add(panel);
-        }
-        in.close();
+//            window.setBounds(500, 150, 300, 200);
+//        }
+//        in.close();
 
-        JLabel ascii = new JLabel(String.valueOf(sb));
-        ascii.setFont(new Font("Monospaced", Font.PLAIN, 12));
-//        ascii.setBorder(BorderFactory.createEmptyBorder(0, 20, 20, 20));
-        ascii.setSize(100, 100);
-        window.getContentPane().add(ascii);
+        ImageIcon splashScreenImage = new ImageIcon("resources/Images/asciiSplashScreen.png");
+        JLabel image = new JLabel(splashScreenImage);
+        window.getContentPane().add(image);
+//        window.setBounds(500, 150, 300, 200);
+//
+//        window.pack();
+//        JLabel ascii = new JLabel(String.valueOf(sb));
+//        ascii.setFont(new Font("Monospaced", Font.PLAIN, 12));
+////        ascii.setBorder(BorderFactory.createEmptyBorder(0, 20, 20, 20));
+//        ascii.setSize(100, 100);
+//        window.getContentPane().add(ascii);
         window.setBounds(500, 150, 300, 200);
-
+//
         window.pack();
-        window.setSize(window.getWidth() * 30, window.getHeight() * 30);
+//        window.setSize(window.getWidth() * 3, window.getHeight() * 3);
         window.setLocationRelativeTo(null);
         window.setVisible(true);
         try {
