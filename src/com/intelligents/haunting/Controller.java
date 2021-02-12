87e732a4ab97a7a8fc524e7ee673ad18a5217d0f
Collin Fairlie <collin.fairlie@gmail.com>
2021-02-12 09:45:35 -0800

    Revised Controller in the easiest way to break up where information is going based off where the player is at or what they are trying to do.

--- src/com/intelligents/haunting/Controller.java
+++ src/com/intelligents/haunting/Controller.java
@@ -6,28 +6,48 @@ public class Controller {
 
 
     private boolean introScreen;
-
-
     private boolean nameSet;
+    private boolean readyToGuess;
     private Game game;
 
     public Controller(Game game) throws IOException {
         introScreen = true;
         nameSet = false;
+        readyToGuess = false;
         this.game = game;
     }
 
-    public void kickoffResponse(String[] response) {
+    public void kickoffResponse(String[] response, String promptToUser) {
+        // Get what chapter the user wants
         if (introScreen) {
             game.intro(response);
             if (response[0].equals("1") || response[0].equals("4")) {
                 introScreen = false;
             }
+        // Gain the users name
         } else if (!introScreen && !nameSet) {
             game.createPlayer(response);
             nameSet = true;
+        // If the user is trying to exit, get if they want to guess or stay inside
+        } else if (promptToUser.equals("It seems your journal does not have all of the evidence needed to determine the ghost." +
+                " Would you like to GUESS the ghost anyway or go back INSIDE?\n>>") || promptToUser.equals("Invalid input, please decide whether you want to GUESS or go back INSIDE.\n>>")
+                || promptToUser.equals("It seems like you could be ready to determine the ghost." +
+                " Would you like to GUESS the ghost or go back INSIDE to continue exploring?\n>>")){
+            if (response[0].contains("guess")) {
+                readyToGuess = true;
+            }
+            game.guessOrGoBackInside(response[0]);
+        // Player has indicated that they are ready to guess
+        } else if (readyToGuess) {
+            game.userGuess(response[0]);
+        // Player has option to write in journal or not
+        } else if (promptToUser.equals("Would you like to document anything in your journal? [Yes/No]\n") || promptToUser.equals("Invalid Journal entry. Please look/show again to document again.\n")) {
+            game.writeEntryInJournal(response[0]);
+        } else if (promptToUser.equals("Your entry:\n>> ")) {
+            System.out.println("HIT");
+            game.inputEntryInJournal(response[0]);
         } else {
-            game.processInput(true, response, 0);
+            game.processInput(true, response, game.attemptCount);
         }
     }
 
